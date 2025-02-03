60730de12d4789e18fb9441665e564e96d06d639
strikan95 <buljevic.juraj@gmail.com>
2025-02-03 18:11:17 +0100

    fixes

--- GPianoRollView.cpp
+++ GPianoRollView.cpp
@@ -1,6 +1,10 @@
 #include "GPianoRollView.h"
 #include "GPianoRollScene.h"
 
+#include <QKeyEvent>
+#include <QScrollBar>
+#include <QMenu>
+
 GPianoRollView::GPianoRollView(QWidget *parent) :
     QGraphicsView(parent)
 {
@@ -29,4 +33,72 @@ GPianoRollView::GPianoRollView(GPianoRollScene *scene, QWidget *parent)
     setSceneRect(0, 0, maxSize * 2, maxSize * 2);
 
     setAlignment(Qt::AlignLeft | Qt::AlignTop);
+}
+
+void GPianoRollView::mousePressEvent(QMouseEvent *e)
+{
+    switch (e->button()) {
+    case Qt::MiddleButton:
+    {
+        setCursor(Qt::ClosedHandCursor);
+        mLastMousePosition = e->position().toPoint();
+        return;
+    }
+    case Qt::LeftButton:
+    {
+        if(e->modifiers() & Qt::ControlModifier)
+        {
+            setDragMode(QGraphicsView::RubberBandDrag);
+            qDebug() << "Setting rubber band drag";
+        }
+    }
+    default:
+        break;
+    }
+
+    QGraphicsView::mousePressEvent(e);
+}
+
+void GPianoRollView::mouseMoveEvent(QMouseEvent *e)
+{
+    if(e->buttons() & Qt::MiddleButton)
+    {
+        QScrollBar *hBar = horizontalScrollBar();
+        QScrollBar *vBar = verticalScrollBar();
+        QPoint delta = e->position().toPoint() - mLastMousePosition;
+        qDebug() << delta;
+        hBar->setValue(hBar->value() + (isRightToLeft() ? delta.x() : -delta.x()));
+        vBar->setValue(vBar->value() - delta.y());
+
+        mLastMousePosition = e->position().toPoint();
+        return;
+    }
+
+    QGraphicsView::mouseMoveEvent(e);
+}
+
+void GPianoRollView::mouseReleaseEvent(QMouseEvent *e)
+{
+    if(e->button() & Qt::MiddleButton)
+    {
+        unsetCursor();
+        return;
+    }
+
+    if(e->button() & Qt::LeftButton && e->modifiers() & Qt::ControlModifier)
+    {
+        setDragMode(QGraphicsView::NoDrag);
+    }
+
+    QGraphicsView::mouseReleaseEvent(e);
+}
+
+void GPianoRollView::keyPressEvent(QKeyEvent *e)
+{
+    QGraphicsView::keyPressEvent(e);
+}
+
+void GPianoRollView::keyReleaseEvent(QKeyEvent *e)
+{
+    QGraphicsView::keyReleaseEvent(e);
 }
--- GPianoRollView.h
+++ GPianoRollView.h
@@ -3,8 +3,18 @@
 
 #include <QGraphicsView>
 #include <QObject>
+#include <QMouseEvent>
+#include <QRubberBand>
 
 class GPianoRollScene;
+
+enum GControlState
+{
+    Default,
+    Pan,
+    RubberBandSelect,
+    VerticalZoom
+};
 
 class GPianoRollView : public QGraphicsView
 {
@@ -12,6 +22,23 @@ class GPianoRollView : public QGraphicsView
 public:
     GPianoRollView(QWidget *parent = Q_NULLPTR);
     GPianoRollView(GPianoRollScene *scene, QWidget *parent = Q_NULLPTR);
+
+protected:
+    void mousePressEvent(QMouseEvent *e) override;
+    void mouseMoveEvent(QMouseEvent *e) override;
+    void mouseReleaseEvent(QMouseEvent *e) override;
+
+    void keyPressEvent(QKeyEvent *e) override;
+    void keyReleaseEvent(QKeyEvent *e) override;
+
+private:
+    void updateControlState(GControlState state) { mCS = state; }
+    GControlState controlState() const { return mCS; }
+
+private:
+    GControlState mCS;
+    std::unique_ptr<QRubberBand> mRubberBand = std::make_unique<QRubberBand>(QRubberBand::Rectangle, this);
+    QPoint mLastMousePosition;
 };
 
 #endif // GPIANOROLLVIEW_H
